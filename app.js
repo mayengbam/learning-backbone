2f9033e158eb0cc3dc5ea8dd07a34a44ee7ccf09
Sushil Mayengbam <memsana.sushil@gmail.com>
2017-02-10 11:44:02 +0530

    Added a collection and collection view

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var Song = Backbone.Model.extend();
+var Playlist = Backbone.Collection.extend({
+    model: Song
+});
+
 var SongView = Backbone.View.extend({
     initialize: function () {
         this.render();
@@ -8,13 +12,37 @@ var SongView = Backbone.View.extend({
         return this;
     }
 });
-
-var song = new Song({
-    title: 'Stairway to Heaven'
+var SongsView = Backbone.View.extend({
+    initialize: function () {
+        this.render();
+    },
+    render: function () {
+        var self = this;
+        this.model.each(function(song){
+            var songView = new SongView({
+                model: song
+            });
+            self.$el.append(songView.render().$el);
+        });
+    }
 });
-var songView = new SongView({
+
+
+var songs = new Playlist([
+    new Song({
+        title: 'A stairway to heaven'
+    }),
+    new Song({
+        title: 'Knocking heaven door'
+    }),
+    new Song({
+        title: 'To be with you'
+    }),
+]);
+var listView = new SongsView({
     el: '#container',
-    model: song
+    model: songs
 });
 
-songView.render();
+
+
