d2255e5abf8fbcee062447f5c86df48bd5a58f7e
Sushil Mayengbam <memsana.sushil@gmail.com>
2017-02-10 11:12:09 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var Song = Backbone.Model.extend();
+var SongView = Backbone.View.extend({
+    initialize: function(){
+        this.render();
+    },
+    render: function(){
+        this.$el.html("Hello, world!!!");
+        return this;
+    }
+});
+
+var song = new SongView({
+    el: '#container'
+});
+    
+song.render();
