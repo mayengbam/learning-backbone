42d88ac936a3e86dde599c31ccf6ef3f230d2473
Sushil Mayengbam <memsana.sushil@gmail.com>
2017-02-10 12:40:21 +0530

    Added Collections event - add/remove model

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ var SongView = Backbone.View.extend({
 var SongsView = Backbone.View.extend({
     initialize: function () {
         this.render();
+        this.model.on('add', this.addSong, this);
+        this.model.on('remove', this.removeSong, this);
+    },
+    addSong: function(song){
+        var songView = new SongView({
+                model: song
+            });
+        this.$el.append(songView.render().$el);
+    },
+    removeSong: function(song){
+        this.$el.empty();
+        this.render();
     },
     render: function () {
         var self = this;
